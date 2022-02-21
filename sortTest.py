d1b473be4c6a0a889cf19fe18c45a4787b893c6b
SirBoopSnoop <60667105+SirBoopSnoop@users.noreply.github.com>
2022-02-21 15:14:22 +0100

    CSV greier
    La til en metode for å hente ut bestemte kolonner fra CSV-fil

---
+++ sortTest.py
@@ -0,0 +1,21 @@
+import csv
+from encodings import utf_8
+line_number1 = 1
+line_number2 = 7
+i = 0
+#   For å skrive flere kolonner til CSV-filen må man legge til en ny line_number variabel med tilsvarende index fra avinor loggen og legge den til text-variabelen
+#   på samme måte som de andre variablene.
+
+with open("logTestClean.csv", 'rt', encoding="utf_8") as f:
+    mycsv = csv.reader(f)
+    mycsv = list(mycsv)
+    testFile = open('testCSV.csv', 'w')
+    for row in mycsv:
+        text = "\"" + (mycsv[i][line_number1]) + "\"" + ", " + "\"" + (mycsv[i][line_number2]) + "\"" + "\n"
+        testFile.write(text)
+        i = i+1
+        #print(text)
+
+testFile.close()
+        
+
