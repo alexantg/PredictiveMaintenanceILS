1ce57d85564acd3d5d4559276796c951fb5f8776
SirBoopSnoop <60667105+SirBoopSnoop@users.noreply.github.com>
2022-03-21 12:12:32 +0100

    Update weatherDataToCSV.py

--- WeatherData/weatherDataToCSV.py
+++ WeatherData/weatherDataToCSV.py
@@ -9,7 +9,7 @@ with open("WeatherData/ENAL20220201000000.csv", 'rt', encoding="utf_8") as f:
     for row in mycsv:
 
         text = str(mycsv[i]).replace(" ", ",")
-        line = (re.search("\d\d/\d\d|\D\d\d/\d\d|\d\d/\D\d\d|\D\d\d/\D\d\d", text)).group().replace(",","").replace("M", "-") + (re.search("\s*[0-9]+", text)).group().replace("Q","") + "\n"
+        line = (re.search("\d\d/\d\d|\D\d\d/\d\d|\d\d/\D\d\d|\D\d\d/\D\d\d", text)).group().replace(",","").replace("M", "-") + "  " + (re.search("Q\d\d\d\d", text)).group().replace("Q","") + "  " + (re.search("(^|=|\s*)\d{4}(\s|$)", text)).group() + "\n"
         testFile.write(line)
         i = i+1
 
