5abbf1d0c5ef5241df4a823f877841655c6fc407
SirBoopSnoop <60667105+SirBoopSnoop@users.noreply.github.com>
2022-03-21 14:16:54 +0100

    Hente relevante verdier fra METAR

--- WeatherData/weatherDataToCSV.py
+++ WeatherData/weatherDataToCSV.py
@@ -6,11 +6,21 @@ with open("WeatherData/ENAL20220201000000.csv", 'rt', encoding="utf_8") as f:
     mycsv = csv.reader(f)
     mycsv = list(mycsv)
     testFile = open('WeatherData/weatherReport.csv', 'w')
+    testFile.write("      DATO     ," + "    TEMP  ," + "  TRYKK," + "  SIKT" + "\n")
     for row in mycsv:
 
-        text = str(mycsv[i]).replace(" ", ",")
-        line = (re.search("\d\d/\d\d|\D\d\d/\d\d|\d\d/\D\d\d|\D\d\d/\D\d\d", text)).group().replace(",","").replace("M", "-") + "  " + (re.search("Q\d\d\d\d", text)).group().replace("Q","") + "  " + (re.search("(^|=|\s*)\d{4}(\s|$)", text)).group() + "\n"
+        text = str(mycsv[i]).replace(",", "").replace("'", "")
+
+        line = ((re.search("\d{4}\s\d{2}\s\d{2}\s\d{2}\s\d{2}", text)).group() + ",  "
+        
+        + (re.search("\d\d/\d\d|\D\d\d/\d\d|\d\d/\D\d\d|\D\d\d/\D\d\d", text)).group().replace(",","").replace("M", "-") + ",  "
+
+        + (re.search("Q\d\d\d\d", text)).group().replace("Q","") + ",  "
+
+        + (re.findall(r"\s[\d]{4}\s", text))[1].replace(",","") + "\n")
+
         testFile.write(line)
+        #testFile.write(text + "\n")
         i = i+1
 
 testFile.close()
