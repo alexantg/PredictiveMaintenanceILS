930cccec5e739bc05cba21f06fa6992a1ec3c41f
SirBoopSnoop <60667105+SirBoopSnoop@users.noreply.github.com>
2022-03-28 13:32:55 +0200

    Endret litt på overføring av metar til logg
    Metar-dataene vil oppdateres med loggverdiene så lenge det finnes metar-data som loggen kan bruke. Mangler logikk som kan sjekke hvilke metar-filer som kan brukes hvis foregående metar-fil ikke har mer data å iterere gjennom.  Trenger også logikk som sørger for at datoene fra loggen og metar stemmer ca. overens

--- WeatherData/attachMetarToLog.py
+++ WeatherData/attachMetarToLog.py
@@ -4,21 +4,24 @@ i = 1
 p = 1
 a = 0
 b = 0
-dataList = []
+
 logFile = open('AnomalyDetection/FormattedData/trainTest.csv', 'a')
 
 with open("WeatherData/weatherReport.csv", 'rt', encoding="utf_8") as f:
     metarCSV = csv.reader(f)
     metarCSV = list(metarCSV)
-    logRead = open('AnomalyDetection/FormattedData/trainTest.csv', 'rt')
+    logRead = open('AnomalyDetection/FormattedData/train.csv', 'rt')
     logCSV = csv.reader(logRead)
     logCSV = list(logCSV)
 
     for row in logCSV:
         #weatherDate = str(metarCSV[p][0]) + " 00"
-        logDate = str(logCSV[i][0]).replace("-"," ").replace(":"," ")
-        weatherValues = str(metarCSV[p][1] + metarCSV[p][2] + metarCSV[p][3])
-        print(weatherValues)
+        logDate = str(logCSV[i][0]).replace("-"," ").replace(":"," ") + ","
+        weatherValues = str(metarCSV[p][1] + "," + metarCSV[p][2] + "," + metarCSV[p][3] + "," + metarCSV[p][4])
+        logValues = str(logCSV[i][1] + "," + logCSV[i][2] + "," + logCSV[i][3] + "," + logCSV[i][4] + "," + logCSV[i][5] + "," + logCSV[i][6] + "," + logCSV[i][7] + "," + logCSV[i][8] + ",")
+        #print(weatherValues)
+
+        allValues = logDate + logValues + weatherValues + "\n"
         #weatherMinute = weatherDate.split()[5]
         logMinute = logDate.split()[4]
         logMinute = int(logMinute)
@@ -28,15 +31,18 @@ with open("WeatherData/weatherReport.csv", 'rt', encoding="utf_8") as f:
             if(a + b == 2):
                 p = p+1
             b = 0
-            dataList.append(weatherValues)
+            #dataList.append(weatherValues)
+            logFile.write(allValues)
 
         if((logMinute > 0 and logMinute < 20) or logMinute > 50):
             b = 1
             if(a + b == 2):
                 p = p+1
             a = 0
-            dataList.append(weatherValues)
+            #dataList.append(weatherValues)
+            logFile.write(allValues)
 
         i=i+1
 
-logFile.write(dataList)
+#logFile.write(dataList)
+logFile.close()
--- WeatherData/weatherDataToCSV.py
+++ WeatherData/weatherDataToCSV.py
@@ -6,18 +6,18 @@ with open("WeatherData/ENAL20220201000000.csv", 'rt', encoding="utf_8") as f:
     mycsv = csv.reader(f)
     mycsv = list(mycsv)
     testFile = open('WeatherData/weatherReport.csv', 'w')
-    testFile.write("      DATO     ," + "    TEMP  ," + "  TRYKK," + "  SIKT" + "\n")
+    testFile.write("DATO," + "TEMP1," + "TEMP2," + "TRYKK," + "SIKT" + "\n")
     for row in mycsv:
 
         text = str(mycsv[i]).replace(",", "").replace("'", "")
 
-        line = ((re.search("\d{4}\s\d{2}\s\d{2}\s\d{2}\s\d{2}", text)).group() + ",  "
+        line = ((re.search("\d{4}\s\d{2}\s\d{2}\s\d{2}\s\d{2}", text)).group() + ","
         
-        + (re.search("\d\d/\d\d|\D\d\d/\d\d|\d\d/\D\d\d|\D\d\d/\D\d\d", text)).group().replace(",","").replace("M", "-") + ",  "
+        + (re.search("\d\d/\d\d|\D\d\d/\d\d|\d\d/\D\d\d|\D\d\d/\D\d\d", text)).group().replace(",","").replace("M", "-").replace("/", ",").replace(" ", "") + ","
 
-        + (re.search("Q\d\d\d\d", text)).group().replace("Q","") + ",  "
+        + (re.search("Q\d\d\d\d", text)).group().replace("Q","") + ","
 
-        + (re.findall(r"\s[\d]{4}\s", text))[1].replace(",","") + "\n")
+        + (re.findall(r"\s[\d]{4}\s", text))[1].replace(",","").replace(" ", "") + "\n")
 
         testFile.write(line)
         #testFile.write(text + "\n")
